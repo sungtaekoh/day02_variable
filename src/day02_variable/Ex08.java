bd2f16f2f77e3fe84a1d2431fe39a9064b0068da
sungtaek <ohbass95@gmail.com>
2021-07-26 14:35:06 +0900

    Quiz02 성적표만들기

--- src/day02_variable/Ex08.java
+++ src/day02_variable/Ex08.java
@@ -8,6 +8,10 @@ public class Ex08 {
 		String n1, n2;
 		System.out.println("첫번째 값 입력 : ");
 		n1 = input.next();
+//		next라는 값은 공백을 줄 수 없다
+//		스페이스바 공백을 쓰게되면 서로 앞의 문자열과 뒤의 문자열을 서로 다른걸로 인식한다
+//		nextline은 엔터값을 가져간다
+//		n2 = input.nextLine(); 공백을 줄 수 있다?
 		System.out.println("두번째 값 입력 : ");
 		n2 = input.next();
 		System.out.println("첫번째 입력 값 : "+n1);
